7b30721f75a180b6d375087114a282f2a8707b9b
DanielaOrd97 <201G0272@rcarbonifera.tecnm.mx>
2024-05-19 19:28:30 -0600

    Log in view

---
+++ ClienteWPF/ViewModels/LogInViewModel.cs
@@ -0,0 +1,40 @@
+﻿using ClienteWPF.Models.DTOs;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+//using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteWPF.ViewModels
+{
+    public partial class LogInViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        private LogInDTO login;
+
+
+        public MainViewModel Mainviewmodel { get; }
+
+        public LogInViewModel(MainViewModel mainviewm)
+        {
+            //service = new();
+            Login = new LogInDTO();
+            Mainviewmodel = mainviewm;
+        }
+
+        [RelayCommand]
+        public async Task LogInn()
+        {
+
+            if (Login != null)
+            {
+                // var response = await service.LogIn(Login);
+                Mainviewmodel.NavegarActividades();
+                //Mainviewmodel.Modo = "actividades";
+            }
+        }
+    }
+}
---
+++ ClienteWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteWPF.ViewModels
+{
+    public partial class MainViewModel:ObservableObject
+    {
+
+        private readonly LogInViewModel loginviewmodel;
+        private readonly ActividadesViewModel actviewmodel;
+
+        //public static string Modo { get; set; }
+        private string modo;
+        public string Modo
+        {
+            get => modo;
+            set => SetProperty(ref modo, value);
+        }
+
+        //private object viewmodelactual;
+
+        //public object ViewModelAactual
+        //{
+        //    get { return viewmodelactual; }
+        //    set { viewmodelactual = value; }
+        //}
+
+        private object viewmodelactual;
+        public object ViewModelAactual
+        {
+            get => viewmodelactual;
+            set => SetProperty(ref viewmodelactual, value);
+        }
+        public MainViewModel()
+        {
+            loginviewmodel = new LogInViewModel(this);
+            actviewmodel = new ActividadesViewModel(this);
+
+            //Modo = "login";
+            ViewModelAactual = loginviewmodel;
+        }
+
+        //[RelayCommand]
+        public void NavegarLogIn()
+        {
+            ViewModelAactual = loginviewmodel;
+        }
+
+        public void NavegarActividades()
+        {
+            //Modo = "actividades";
+            ViewModelAactual = actviewmodel;
+        }
+    }
+}
